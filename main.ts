cb1ac597f913c744d5204aa779a9dbf2e45a10d4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-27 15:49:35 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 99; index++) {
-        whaleysans.showNumber(index)
-        for (let loop1 = 0; loop1 <= 4; loop1++) {
-            for (let loop2 = 0; loop2 <= 4; loop2++) {
-                if (led.pointBrightness(loop1, loop2) > 0) {
-                    colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Red))
-                } else {
-                    colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Black))
-                }
-            }
-        }
-        basic.pause(100)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
+    proportionalFont.showString("To Be Or Not To Be", 150)
+    proportionalFont.showSpace(3, 150)
     colorbit_51bit.clear()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.clearScreen()
 })
 let colorbit_51bit: colorbit.Strip = null
 colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(16)
 colorbit_51bit.clear()
-led.enable(true)
+led.enable(false)
 colorbit_51bit.show()
+basic.forever(function () {
+    for (let loop1 = 0; loop1 <= 4; loop1++) {
+        for (let loop2 = 0; loop2 <= 4; loop2++) {
+            if (led.pointBrightness(loop1, loop2) > 0) {
+                colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Red))
+            } else {
+                colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Black))
+            }
+        }
+    }
+})
