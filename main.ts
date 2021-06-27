4a348b05d6455610de5299773a0d11ed707c0e58
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-27 15:00:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    whaleysans.showNumber(85)
+    whaleysans.showNumber(54)
 })
 input.onButtonPressed(Button.AB, function () {
     colorbit_51bit.clear()
@@ -15,7 +15,7 @@ input.onButtonPressed(Button.B, function () {
     for (let loop1 = 0; loop1 <= 4; loop1++) {
         for (let loop2 = 0; loop2 <= 4; loop2++) {
             if (led.pointBrightness(loop1, loop2) > 0) {
-                colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.White))
+                colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Red))
             } else {
                 colorbit_51bit.drawColorBit(loop1, loop2, colorbit.colors(BitColors.Black))
             }
@@ -24,5 +24,7 @@ input.onButtonPressed(Button.B, function () {
 })
 let colorbit_51bit: colorbit.Strip = null
 colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
+colorbit_51bit.setBrightness(16)
 colorbit_51bit.clear()
-led.enable(false)
+led.enable(true)
+colorbit_51bit.show()
